1340fb008a4341a70641e9fe595b25079f5197f1
krishna-s11 <krishnasaxena69@gmail.com>
2023-12-22 11:34:00 +0530

    about img

--- pages/about/index.js
+++ pages/about/index.js
@@ -111,7 +111,7 @@ const About = () => {
         initial='hidden'
         animate='show'
         exit='hidden'
-        className='hidden xl:flex absolute bottom-0 -left-[470px]'
+        className='hidden xl:flex absolute bottom-0 -left-[570px]'
       >
         <Avatar />
       </motion.div>
